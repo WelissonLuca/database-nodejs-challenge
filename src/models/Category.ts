f6e0b258d6ea54922778bac6ffea5a5bd72cd112
Welisson luca <welissonluca17@gmail.com>
2021-08-19 00:07:06 -0300

    feat: add entity category and transactions

--- src/models/Category.ts
+++ src/models/Category.ts
@@ -1,11 +1,29 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { v4 as uuid } from 'uuid';
+
+@Entity('categories')
 class Category {
+  @PrimaryColumn()
   id: string;
 
+  @Column()
   title: string;
 
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
+
+  constructor() {
+    this.id = this.id ?? uuid();
+  }
 }
 
 export default Category;
--- src/models/Transaction.ts
+++ src/models/Transaction.ts
@@ -1,17 +1,45 @@
+import {
+  Column,
+  CreateDateColumn,
+  Entity,
+  JoinColumn,
+  ManyToOne,
+  PrimaryColumn,
+  UpdateDateColumn,
+} from 'typeorm';
+import { v4 as uuid } from 'uuid';
+import Category from './Category';
+
+@Entity('transactions')
 class Transaction {
+  @PrimaryColumn()
   id: string;
 
+  @Column()
   title: string;
 
+  @Column()
   type: 'income' | 'outcome';
 
+  @Column()
   value: number;
 
+  @Column()
   category_id: string;
 
+  @ManyToOne(() => Category)
+  @JoinColumn({ name: 'category_id' })
+  category: Category;
+
+  @CreateDateColumn()
   created_at: Date;
 
+  @UpdateDateColumn()
   updated_at: Date;
+
+  constructor() {
+    this.id = this.id ?? uuid();
+  }
 }
 
 export default Transaction;
